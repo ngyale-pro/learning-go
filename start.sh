fa49e947975a539e3ff379122fb78b8150cf6eca
ngyale-pro <alexandrehoa.nguyen.pro@protonmail.com>
2023-08-18 16:18:54 +0200

    deploy conf: load env

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -e
 
 
 echo "---------- Run DB migration ----------"
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 echo "---------- Start application ----------"
 exec "$@" # Takes all parameter as input and execute it
