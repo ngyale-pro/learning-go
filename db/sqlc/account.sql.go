7953d0c0403208e8b2d47231ef408aadd4897a59
Alexandre Nguyen <alexandrehoa.nguyen.pro@protonmail.com>
2023-09-12 16:17:05 +0200

    rerun

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -114,7 +114,7 @@ const listAccounts = `-- name: ListAccounts :many
 SELECT id, owner, balance, currency, created_at FROM accounts
 WHERE owner = $1
 ORDER BY id
-LIMIT $2 
+LIMIT $2
 OFFSET $3
 `
 
